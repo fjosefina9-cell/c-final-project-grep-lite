896d759dc9da9b25e733a3191f2c7f2497138d84
Marc Martinez <mmartinez936@toromail.csudh.edu>
2026-05-04 16:36:49 -0700

    Add MAX_FILES and extend Args struct for multiple files and flags

--- include/args.h
+++ include/args.h
@@ -3,12 +3,16 @@
 #ifndef ARGS_H
 #define ARGS_H
 
-/* placeholder for argument parsing functions */
+#define MAX_FILES 100
+
 struct Args {
 	int ignore_case;
 	int whole_word;
+	int line_numbers;
+    int count_only;
 	const char *pattern;
-	const char *filename;
+	const char *files[MAX_FILES];
+    int file_count;
 };
 	
 int parse_args(int argc, char *argv[], struct Args *args);
